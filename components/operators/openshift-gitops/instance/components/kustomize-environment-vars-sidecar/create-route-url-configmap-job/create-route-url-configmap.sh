06e3f49d091d2bd0a409116b8fedc5d1fdae6611
Trevor Royer <troyer@redhat.com>
2024-11-08 15:38:52 -0700

    add check if cm already exists

--- components/operators/openshift-gitops/instance/components/kustomize-environment-vars-sidecar/create-route-url-configmap-job/create-route-url-configmap.sh
+++ components/operators/openshift-gitops/instance/components/kustomize-environment-vars-sidecar/create-route-url-configmap-job/create-route-url-configmap.sh
@@ -7,5 +7,21 @@ APP_URL=$(oc get ingresses.config/cluster -o jsonpath={.spec.domain})
 
 echo
 echo "App URL: ${APP_URL}"
-echo "Creating plugin-substitution-env-vars configmap"
-oc create configmap plugin-substitution-env-vars --from-literal=SUB_DOMAIN=${APP_URL}
+
+CM_NAME="plugin-substitution-env-vars"
+TARGET_NAMESPACE="openshift-gitops"
+
+echo
+echo "Checking if configmap ${CM_NAME} already exists"
+
+if oc get configmap ${CM_NAME} -n ${TARGET_NAMESPACE} --ignore-not-found | grep ${CM_NAME}; then
+    echo "Configmap ${CM_NAME} already exists. Skipping setup."
+else
+    echo "Creating ${CM_NAME} configmap"
+    oc create configmap ${CM_NAME} --from-literal=SUB_DOMAIN=${APP_URL} -n ${TARGET_NAMESPACE}
+
+    echo
+    echo "Restarting repo server pod"
+    sleep 10
+    oc delete pods -l app.kubernetes.io/name=openshift-gitops-repo-server -n ${TARGET_NAMESPACE}
+fi
